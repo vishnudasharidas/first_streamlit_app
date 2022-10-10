80f7b2a1b967c15c02fe37e3e3003958009b1be4
Vishnu Das <115206952+vishnudasharidas@users.noreply.github.com>
2022-10-10 10:35:21 +0530

    Update streamlit_app.py
    Pick Some

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ streamlit.text("🥝 Orange creamsicle protein shake")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
+
 
 
 

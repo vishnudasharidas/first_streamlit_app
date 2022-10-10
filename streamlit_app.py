6d4955539e7adabe42207dd11436a6ae38241884
Vishnu Das <115206952+vishnudasharidas@users.noreply.github.com>
2022-10-10 10:47:31 +0530

    Update streamlit_app.py
    Locate Fruit selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ streamlit.text("🥝 Orange creamsicle protein shake")
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Avocado'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Apple', 'Avocado'])
+fruits_selected = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_selected)
 
 
 

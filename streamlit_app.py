42dbe6f7289e784eb8e06e794a3f510090dc16ee
Vishnu Das <115206952+vishnudasharidas@users.noreply.github.com>
2022-10-13 14:52:49 +0530

    Update streamlit_app.py
    json Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_selected)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
